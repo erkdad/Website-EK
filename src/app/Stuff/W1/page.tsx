1ba38ed625005deb9b6501ad274fa44851e4524c
Eric Kennes <erickennes9@gmail.com>
2025-05-20 23:18:37 -0400

    Go Back to W1

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -3,13 +3,6 @@ export default function W1() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
-        <Image
-          src="/images/W1_Camp island 1.jpg"
-        />
-        <Image
-          width="max"
-          src="/images/W1_Camp island 2.jpg"
-        />
       </div>
     </main>
   );
