6b1ab917d659bacd0e55ff06f8519296feb9eea7
Eric Kennes <erickennes9@gmail.com>
2025-05-20 22:14:19 -0400

    Message test

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -1,9 +1,13 @@
-export default function Videos() {
+export default function W1() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">This is my first week's post on the website</p>
-
+        <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
+        <iframe
+          width="max"
+          height="500"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
       </div>
     </main>
   );
