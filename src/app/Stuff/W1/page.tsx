45c4880723b8f8539fcbd48fd1c980199334e97e
Eric Kennes <erickennes9@gmail.com>
2025-05-20 23:35:27 -0400

    Super try

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -3,7 +3,7 @@ export default function W1() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
-        <img src="images/W1_Camp Island 2" style="width: 100%; height: auto;"></img>
+        <img src="public\images\W1_Camp Island 2.png" style="width: 100%; height: auto;"></img>
       </div>
     </main>
   );
