fc7f6c87b1f4635392f9c5ad6e85e69c56ed9e43
Eric Kennes <erickennes9@gmail.com>
2025-05-21 00:16:44 -0400

    a

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -1,14 +1,16 @@
+import Image from "next/image";
+
 export default function W1() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
-                    <Image
-                      src="/images/W1x2.png"
-                      alt="other"
-                      width={256}
-                      height={256}
-                    />
+          <Image
+           src="/images/W1x2.png"
+           alt="other"
+          width={100%}
+          height={100%}
+          />
       </div>
     </main>
   );
