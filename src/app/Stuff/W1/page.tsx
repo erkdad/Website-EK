599b6b14b4387ad039f9bb6e6c4621ba49887e59
Eric Kennes <erickennes9@gmail.com>
2025-05-21 00:10:14 -0400

    sq1

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -3,12 +3,6 @@ export default function W1() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
-        <Image
-              src="/images/W1x2.jpg"
-              alt="First Camp"
-              width = {100%}
-
-        />
       </div>
     </main>
   );
