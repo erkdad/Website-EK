0d6467db391c4bd6046b3c665037d08cf6059fb2
Eric Kennes <erickennes9@gmail.com>
2025-05-21 20:55:01 -0400

    w1 text

--- src/app/Stuff/W1/page.tsx
+++ src/app/Stuff/W1/page.tsx
@@ -4,13 +4,33 @@ export default function W1() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">Week 1 Text goes here</p>
-          <Image
-           src="/images/W1x2.png"
-           alt="other"
-          width={100%}
-          height={100%}
-          />
+        <p className="text-xl leading-relaxed">After a long hiatus of website dormancy, 
+          I hope to contribute to the "STUFF" on a somewhat consitent basis. W1 or "Week 1" 
+          has got me trying out this website code again, something i dont have really any experience 
+          in, to prove i can do it. last night i was up until 12:30 trying to put a dang image on this page, 
+          and im still trying. If it isnt here when you see this, i still havent figured it out.
+        </p>
+        <p className="text-xl leading-relaxed"> Last week had my family up visiting the cottage in thunder beach, 
+          a 24 hour stake on an island, and some runs and bikes up in the trails here. On one of my runs in the 
+          Dorian trails, i realized the path was meant for mountain bikes, fast moutain bikes. Soon enough
+          a guy comes ripping down the trail in front of me and i say "Sick"; he says "thanks". I got out of his way.
+          After about a half an hour, the dude passes me again and this time i gotta ask him about the trail and how 
+          it is on a bike. He stopped and we chatted for about 10 minutes as he lived really close to the spot we 
+          are renting. He showed me this app called trailforks and its usability to find trails anywhere. You never know
+          when youre going to recieve the most crucial tips for your own life. Explore more, try a new trail, and talk
+          to the random guy whos passing you.
+        </p>
+        <p className="text-xl leading-relaxed"> One more note i'll leave here is my recent appreciation for a meeting i had
+          with a random runner at the running store a week before my ultra. Josh Lynott is an aussie that was doing a book 
+          tour in canada and had a stop in london. He wrote a book that is poetry for runners; "a note to the runners". 
+          He let me borrow his salomon shorts so i could try these shoes i never wore before i strapped them up for a big one.
+          It was only us in the store so he didnt really mind me using his equipment before his running group arrived. 
+          We got to chatting about gear and runs we hope to do in the future. After leaving the store i looked him up and he was 
+          decenlty famous online, with a good following, and a podcast. I found that meeting him randomly, and then finding his 
+          online content relatable, gave me a source of inspiration i havent had in a while.
+
+          talk to the random guy who is passing you. 
+        </p>
       </div>
     </main>
   );
