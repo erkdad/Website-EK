41cf321424d9d8806f687799d2be5cf490611fee
Eric Kennes <erickennes9@gmail.com>
2025-05-28 20:38:25 -0400

    W2 stuff 1

--- src/app/Stuff/W2/page.tsx
+++ src/app/Stuff/W2/page.tsx
@@ -4,9 +4,32 @@ export default function W2() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">this is live
+        <p className="text-xl leading-relaxed">Week 2!!</p>
+
+        <p className="text-xl leading-relaxed">This week is out last week at our spot in Tiny; Thunder Beach has been nice to us. Cant believe
+          how fast these 5 month went by. Feels like Jan 1st was yesterday. Through all the snow storms, rainy days, and gorgeous sunny ones, this place built character.
+          i am gonna miss being surrounded by wood, and feeling the airy high ceilings. We were living like royalty in 2025. Next stop Collingwood! 
+
         </p>
-        <p> <img src="/images/W2/paint1_phone.jpg" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed"> We had one last friend hangout on the weekend where some homies came up to ride bikes, chill lakeside, look at birds, and drink some pints.
+        My first time seeing Henry in almost a year! he told us about his birding background and his sighting of the Quebecois *******. The crowd lit up on that one. found the sunshine on saturday
+        evening after a rainy predicted weekend!</p>
+
+        <p> <img src="/images/W2/Fwends.jpg" alt="Description of Image6"></img></p>
+        p> <img src="/images/W2/F1.jpg" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed">Wanted to go out an paint a sloping landscape with Willamina. Her first Plein air!</p>
+        <p> <img src="/images/W2/Paint1.jpg" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed">kept up the stoke and tried want to paint the church in Lafontaine. Ended up finding this truck parcked out back and couldnt help it.</p>
+        <p> <img src="/images/W2/Paint2.jpg" alt="Description of Image6"></img></p>
+
+
+        Also scanned some drawings 
+        <p> <img src="/images/W2/SF Streetzjpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W2/Bins.jpg" alt="Description of Image6"></img></p>
+
         <p className="text-xl leading-relaxed"> 05.28.2025 EK </p>
       </div>
     </main>
