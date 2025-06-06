527dd0a4a3876683e21d105a9e052501c90052fb
Eric Kennes <erickennes9@gmail.com>
2025-06-06 11:14:44 -0400

    Trial4

--- src/app/Stuff/page.tsx
+++ src/app/Stuff/page.tsx
@@ -8,7 +8,6 @@ export default function Stuff() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px]">
-          <div className="flex flex-col items-center">
             <Image
               src="/images/VideoIMG.svg"
               alt="Stuff"
