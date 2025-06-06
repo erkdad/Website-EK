e297241e7bee9fc7c25a0d10fc46488d2b32ba84
Eric Kennes <erickennes9@gmail.com>
2025-06-05 20:43:23 -0400

    RUN ITTTT

--- src/app/Stuff/W3/page.tsx
+++ src/app/Stuff/W3/page.tsx
@@ -6,6 +6,42 @@ export default function W3() {
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 3 *Yussss*</p>
 
+        <p className="text-xl leading-relaxed">The past week has been filled with moving parts, stalled cars, and fast bikes.
+          on the way back to my hometown this weekend, my car stalled and wouldnt turn back on. luckily we were close to home and i could get the car towed and
+          a ride for willamina and i. The car was new to me 2 months ago so the warranty will cover the damages as i know now. My motorcycle came out
+          of the garage for good this weekend with some good riding down country roads and into the city. Going fast on 2 wheels is a high i hope to keep for a 
+          long time coming.
+        </p>
+        <p> <img src="/images/W3/BIKES.png" alt="Description of Image6"></img></p>
+        <p className="text-xl leading-relaxed"> BIKES YA HEARD
+        </p>
+
+
+        <p className="text-xl leading-relaxed">how can you write more? why is it hard to write?</p>
+        <p className="text-xl leading-relaxed">Writing can be hard sometimes. Whether you feel like 
+          what youre writing isnt worth recording, or that your mind has a tough time putting words 
+          to the emotions you felt. this is espacially true if the time between journaling and the 
+          events is far apart. Emotions eb and flow, change directions, and are impacted 
+          by new stimulus. The thoughts you have today are likely to be somewhat altered tomorrow.</p>
+          <p className="text-xl leading-relaxed">why should you care about this? </p>
+          <p className="text-xl leading-relaxed">To me, i hope that i can remember life how i lived it; the feelings, 
+            the wonder, the unexpected beauties, and especially the shared connections. Recording this gives attention
+             to your lifes course and lets you look back intentionally. The idea of living a life that has a story to
+              read back on sounds more tangible to me. I myself feel sad when i cant recall what i did during the past
+               week; it feels wasted as there are no memories attached to it.</p>
+               <p className="text-xl leading-relaxed">How can i write more without thinking about it?</p>
+               <p className="text-xl leading-relaxed">Set up recording devices in your immediate vicinity;
+                 bring a small journal with you in your pocket, have voice records in your phone easily accessible. 
+                 have a note pad open on your computer while your working, take pictures on you phone liberously, 
+                 and save the articles that interest you. These are all ways of remembering things you encounter in 
+                 your life. you can write down a couple lines in your book in the park, while your waiting in line, 
+                 or at the coffee meetup. Record your voice on a run or while on a walk in the forest. Take a picture 
+                 on your phone of the book your friend told you about. Draw memories in and always feel that your actions 
+                 are worth recording, the memories are truely pricless. </p>
+               <p className="text-xl leading-relaxed">I dont mean to write down everything i do. I just want the 
+                access to record immediatley if i feel the urge to, rather than having to dig up an old book to write days 
+                later. just like bringing a water bottle on a hike incase you get thirsty. </p>
+               <p className="text-xl leading-relaxed">why should you care about this? </p>
 
 
         <p className="text-xl leading-relaxed"> 06.05.2025 EK </p>
