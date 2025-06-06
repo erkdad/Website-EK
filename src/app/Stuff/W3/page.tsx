efac34df521ad627af7e641fb19b9ca3a0ec6c59
Eric Kennes <erickennes9@gmail.com>
2025-06-05 21:06:01 -0400

    fINAL

--- src/app/Stuff/W3/page.tsx
+++ src/app/Stuff/W3/page.tsx
@@ -7,8 +7,8 @@ export default function W3() {
         <p className="text-xl leading-relaxed">Week 3 *Yussss*</p>
 
         <p className="text-xl leading-relaxed">The past week has been filled with moving parts, stalled cars, and fast bikes.
-          on the way back to my hometown this weekend, my car stalled and wouldnt turn back on. luckily we were close to home and i could get the car towed and
-          a ride for willamina and i. The car was new to me 2 months ago so the warranty will cover the damages as i know now. My motorcycle came out
+          On the way back to my hometown this weekend, my car stalled and wouldnt turn back on. Luckily we were close to home and i could get the car towed and
+          a ride for willamina and i. The car was new to me 2 months ago, so the warranty will cover the damages as i know now. My motorcycle came out
           of the garage for good this weekend with some good riding down country roads and into the city. Going fast on 2 wheels is a high i hope to keep for a 
           long time coming.
         </p>
@@ -18,9 +18,12 @@ export default function W3() {
         <p className="text-xl leading-relaxed"> WILLI AND I MADE IT TO DELAWARE and WENT TO MOMS BARN
         </p>
         <p> <img src="/images/W3/Willi2.png" alt="Description of Image6"></img></p>
+        <p className="text-xl leading-relaxed">ON OUR DRIVE WE SAW THIS COOL HOUSE! DREAMING OF A COMPOUND IN THE FOREST</p>
 
+        <p> <img src="/images/W3/COOLHOUSE.png" alt="Description of Image6"></img></p>
 
-        <p className="text-xl leading-relaxed">how can you write more? why is it hard to write?</p>
+        <p className="text-xl leading-relaxed">With this lovely chaotic week, i thought of some questions to ponder about writing;</p>
+        <p className="text-xl leading-relaxed">how can you write more? why is it hard to journal?</p>
         <p className="text-xl leading-relaxed">Writing can be hard sometimes. Whether you feel like 
           what youre writing isnt worth recording, or that your mind has a tough time putting words 
           to the emotions you felt. this is espacially true if the time between journaling and the 
@@ -34,7 +37,7 @@ export default function W3() {
                week; it feels wasted as there are no memories attached to it.</p>
                <p className="text-xl leading-relaxed">How can i write more without thinking about it?</p>
                <p className="text-xl leading-relaxed">Set up recording devices in your immediate vicinity;
-                 bring a small journal with you in your pocket, have voice records in your phone easily accessible. 
+                 bring a small journal with you in your pocket, have voice records in your phone easily accessible, 
                  have a note pad open on your computer while your working, take pictures on you phone liberously, 
                  and save the articles that interest you. These are all ways of remembering things you encounter in 
                  your life. you can write down a couple lines in your book in the park, while your waiting in line, 
@@ -44,7 +47,9 @@ export default function W3() {
                <p className="text-xl leading-relaxed">I dont mean to write down everything i do. I just want the 
                 access to record immediatley if i feel the urge to, rather than having to dig up an old book to write days 
                 later. just like bringing a water bottle on a hike incase you get thirsty. </p>
-               <p className="text-xl leading-relaxed">have a good week!</p>
+
+
+               <p className="text-xl leading-relaxed">have a good week! PCE N LUV</p>
 
 
         <p className="text-xl leading-relaxed"> 06.05.2025 EK </p>
