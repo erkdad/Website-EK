68f66e095da70302004bdd698fc8f82354089b8e
Eric Kennes <erickennes9@gmail.com>
2025-06-10 15:43:54 -0400

    up the PIXXXX

--- src/app/Stuff/W4/page.tsx
+++ src/app/Stuff/W4/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function W3() {
   return (
     <main className="flex w-full">
-      <div className="flex w-full max-w-[1080px] flex-col space-y-6">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Week 4 Coming Soon</p>
 
         
