ab63d7f5bcdf059d66a3a9533dcda25b94ce2f24
Eric Kennes <erickennes9@gmail.com>
2025-06-13 16:14:13 -0400

    try 1

--- src/app/Stuff/W4/page.tsx
+++ src/app/Stuff/W4/page.tsx
@@ -4,11 +4,28 @@ export default function W3() {
   return (
     <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">Week 4 Coming Soon</p>
+        <p className="text-xl leading-relaxed">Week 4 *smile* </p>
 
+        <p className="text-xl leading-relaxed"> TIME IS THE MOST VALUABLE CURRENCY. I CAN PROVE IT.
+        </p>
+
+        <p className="text-xl leading-relaxed">This week i learned how to plan 
+          for busy times and prepare for things. when approaching a layered timeline, i need to 
+          make notes of items i should remember |like a bike helmet or my painting kit| and not rush to the scene. everything takes TIME, and should honour each step of the process if i know what i am approaching will be large. 
+        </p>
         
+        time on the bike
+        <p> <img src="/images/W4/DSCF7134.jpg" alt="Description of Image6"></img></p>
+        
+        
+        <p className="text-xl leading-relaxed"> Expected time 1:00:00 Gotten Time 1:01:15, 1 minute 15 seconds of time to realize we want to do this again.
+          1 hour 1 minute 15 seconds of joggin with my firend Sam. PRICELESS.
+        </p>
+
+        <p> <img src="/images/W4/DSCF7154.jpg" alt="Description of Image6"></img></p>
+
+
         <p> <img src="/images/W4/solarwind_project.jpg" alt="Description of Image6"></img></p>
-        
         <p className="text-xl leading-relaxed"> 06.12.2025 EK </p>
       </div>
     </main>
