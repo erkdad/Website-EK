47449e0bad08f2be07775486d39f09f0e6bcb60e
Eric Kennes <erickennes9@gmail.com>
2025-06-13 16:25:32 -0400

    Push 2

--- src/app/Stuff/W4/page.tsx
+++ src/app/Stuff/W4/page.tsx
@@ -4,17 +4,19 @@ export default function W3() {
   return (
     <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">Week 4 *smile* </p>
+        <p className="text-xl leading-relaxed">Week 4 *smile* *take a breath*</p>
 
-        <p className="text-xl leading-relaxed"> TIME IS THE MOST VALUABLE CURRENCY. I CAN PROVE IT.
+        <p className="text-xl leading-relaxed"> TIME IS THE MOST VALUABLE CURRENCY. I CAN PROVE IT. it is finite, non-renewable, and essential for all human endeavors.
+          i appreciate time, i want to give time.
         </p>
 
-        <p className="text-xl leading-relaxed">This week i learned how to plan 
-          for busy times and prepare for things. when approaching a layered timeline, i need to 
-          make notes of items i should remember |like a bike helmet or my painting kit| and not rush to the scene. everything takes TIME, and should honour each step of the process if i know what i am approaching will be large. 
+        <p className="text-xl leading-relaxed">This week i learned how to plan and prepare for things. when approaching a layered timeline, i need to 
+          make notes of items i should remember |where im at| and not rush to the scene. everything takes TIME, and i should honour each step of the process 
+          if i know what i am approaching will be large. this week i am back in motion up in collignwood. Soon i will be in the flow of the place and the people around me. 
         </p>
         
-        time on the bike
+        <p className="text-xl leading-relaxed">From back home;
+        </p>
         <p> <img src="/images/W4/DSCF7134.jpg" alt="Description of Image6"></img></p>
         
         
@@ -23,9 +25,14 @@ export default function W3() {
         </p>
 
         <p> <img src="/images/W4/DSCF7154.jpg" alt="Description of Image6"></img></p>
-
+       
+        <p className="text-xl leading-relaxed"> Pushing for inspiration within my field and dreaming large!
+        </p>
 
         <p> <img src="/images/W4/solarwind_project.jpg" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed"> What a crazy ride it is to be human! </p>
+
         <p className="text-xl leading-relaxed"> 06.12.2025 EK </p>
       </div>
     </main>
