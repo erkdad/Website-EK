9792ed393cdf2fe85407939e55ccbffdc93f9a2a
Eric Kennes <erickennes9@gmail.com>
2025-06-18 17:23:04 -0400

    W5 Add

--- src/app/Stuff/W4/page.tsx
+++ src/app/Stuff/W4/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function W3() {
+export default function W4() {
   return (
     <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
---
+++ src/app/Stuff/W5/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function W5() {
+  return (
+    <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+        <p className="text-xl leading-relaxed">Week 5 *Turn up phu*</p>
+
+        
+        <p className="text-xl leading-relaxed"> 06.18.2025 EK </p>
+      </div>
+    </main>
+  );
+}
