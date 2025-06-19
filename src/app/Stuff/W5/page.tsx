43420523564f12fd14bf312832f314d84b452778
Eric Kennes <erickennes9@gmail.com>
2025-06-18 22:23:21 -0400

    w5 launch

--- src/app/Stuff/W5/page.tsx
+++ src/app/Stuff/W5/page.tsx
@@ -2,11 +2,23 @@ import Image from "next/image";
 
 export default function W5() {
   return (
-    <main className="flex w-full">
-      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
-        <p className="text-xl leading-relaxed">Week 5 *Turn up phu*</p>
+        <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
+      <div className="flex w-full max-w-[1080px] flex-col space-y-6">
+        <p className="text-xl leading-relaxed">Week 5 *woooo* *didnt spell check*</p>
 
-        
+        <p className="text-xl leading-relaxed">this week was a full load type week; well more particularily the thursday-saturday time frame. I had a very short attention span to those around me and felt like a couldnt make descisions without stressing over the outcomes. yet, in my normal day-to-day life, i take pride in going with the flow and being able to adjust to different social scenarios.
+        </p>
+
+        <p className="text-xl leading-relaxed">So why couldnt i operate in any respectable fashion? well, i delved into the core root of the issues at mind, which led me to discover parts theory. this is something that ive been aware of before, but never fully appreciated its role in probelm solving. Specifically, why am i reacting to certain scenarios with stress or confusion? how can i look within myself to approach the situation with the best version of me?</p>
+
+        <p className="text-xl leading-relaxed">ultimatly, this is to help others in social dynamics where i act poorly or unadhering to their needs. it is to give myself a new toolbox to diagnose emotional movement within the mind and give reason to the decisions i could be making</p>
+        <p className="text-xl leading-relaxed">some direct information from the first source i found online. this will jumpstart my thought process and let the navigation take precedent in times of need!</p>
+        <p className="text-xl leading-relaxed">the below statments are from this link. https://www.psychologytoday.com/ca/blog/making-the-whole-beautiful/202202/how-parts-work-helps-us-get-to-know-ourselves#:~:text=Parts%20Work%20is%20a%20therapeutic,of%20our%20internal%20psychological%20landscape.</p>
+        <p className="text-xl leading-relaxed">each of us has many different parts to our minds and psyches. Each of these parts (or subpersonalities) has unique needs, wants, and beliefs and may be consciously or unconsciously helping or harming us as we move through our days encountering different situations, triggers, and scenarios.</p>
+        <p className="text-xl leading-relaxed">By bringing our awareness to these parts we can then give each a voice, learn what each needs, wants, and fears. This can help us understand more deeply why each gets triggered</p>
+
+              <p> <img src="/images/W5/MovinMelodies.jpg" alt="Description of Image6"></img></p>
+        <p className="text-xl leading-relaxed"> actually sounding like im taking off into the 5d dimension of dance and groove. give a listen when youre muddin</p>
         <p className="text-xl leading-relaxed"> 06.18.2025 EK </p>
       </div>
     </main>
