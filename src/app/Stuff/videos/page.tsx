7ba5f0e6af5e4ea9229c54c81aba2fc9d016955e
Eric Kennes <erickennes9@gmail.com>
2025-06-19 12:24:29 -0400

    cmon

--- src/app/Stuff/videos/page.tsx
+++ src/app/Stuff/videos/page.tsx
@@ -6,7 +6,7 @@ export default function Videos() {
         <iframe
           width="max"
           height="500"
-          src="https://www.youtube.com/embed/k9Oik67BATM&t"
+          src="https://www.youtube.com/embed/k9Oik67BATM&pp"
         ></iframe>
       </div>
     </main>
