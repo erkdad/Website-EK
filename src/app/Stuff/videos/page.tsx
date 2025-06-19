ac5265acfa9f4573d8ad947e484530c7d6398d3c
Eric Kennes <erickennes9@gmail.com>
2025-06-19 12:10:01 -0400

    Basic Video

--- src/app/Stuff/videos/page.tsx
+++ src/app/Stuff/videos/page.tsx
@@ -3,13 +3,11 @@ export default function Videos() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Videos: MAYBE A WATCHLIST</p>
-        <iframe width={560} height={315} 
-        src="https://youtu.be/k9Oik67BATM?si=DunPx_J_tNxUsghK" 
-        title="YouTube video player" 
-        frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-      </iframe>
+        <iframe
+          width="max"
+          height="500"
+          src="https://youtu.be/k9Oik67BATM?si=DunPx_J_tNxUsghK"
+        ></iframe>
       </div>
     </main>
   );
