6e712520383d5ccd4d1623c0d2eb40b3c1c474c2
Eric Kennes <erickennes9@gmail.com>
2025-06-19 11:59:46 -0400

    New video push

--- src/app/Stuff/videos/page.tsx
+++ src/app/Stuff/videos/page.tsx
@@ -3,11 +3,13 @@ export default function Videos() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Videos: MAYBE A WATCHLIST</p>
-        <iframe
-          width="max"
-          height="500"
-          src="https://www.youtube.com/watch?v=k9Oik67BATM&t=31s"
-        ></iframe>
+        <iframe width="560" height="315" 
+        src="https://youtu.be/k9Oik67BATM?si=DunPx_J_tNxUsghK" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+      </iframe>
       </div>
     </main>
   );
