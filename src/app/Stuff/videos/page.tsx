1cab5c1f31a698ecd88fb7247377d81cdeff4df5
Eric Kennes <erickennes9@gmail.com>
2025-06-19 12:06:56 -0400

    pls

--- src/app/Stuff/videos/page.tsx
+++ src/app/Stuff/videos/page.tsx
@@ -3,7 +3,7 @@ export default function Videos() {
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
         <p className="text-xl leading-relaxed">Videos: MAYBE A WATCHLIST</p>
-        <iframe width="560" height="315" 
+        <iframe width={560} height={315} 
         src="https://youtu.be/k9Oik67BATM?si=DunPx_J_tNxUsghK" 
         title="YouTube video player" 
         frameBorder="0" 
