6d6c7779d4ca793f0192efacd8517cbce8d75392
Eric Kennes <erickennes9@gmail.com>
2025-06-26 09:16:30 -0400

    w6 fin

--- src/app/Stuff/W6/page.tsx
+++ src/app/Stuff/W6/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function W5() {
+export default function W6() {
   return (
     <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
@@ -9,7 +9,14 @@ export default function W5() {
       <p> <img src="/images/W6/DSCF7235.jpg" alt="Description of Image6"></img></p>
       <p> <img src="/images/W6/DSCF7239 - Copy.jpg" alt="Description of Image6"></img></p>
       <p> <img src="/images/W6/BlueGrass.jpg" alt="Description of Image6"></img></p>
-      <p> <img src="/images/W6/DSCF7238 (1).jpg" alt="Description of Image6"></img></p>
+      <p> <img src="/images/W6/DSCF7223 - Copy (1).jpg" alt="Description of Image6"></img></p>
+      <p> <img src="/images/W6/Thorbury.jpg" alt="Description of Image6"></img></p>
+       <p className="text-xl leading-relaxed"> I even got my substack feed rolling in good
+        insporational news. The Weekly Anthropocene, Your Daily Dose of Climate Hope, RunningSupply, and Volts got me started.
+
+        </p>
+        <p className="text-xl leading-relaxed"> 06.26.2025 EK . Stayin curious!
+        </p>
       </div>
     </main>
   );
