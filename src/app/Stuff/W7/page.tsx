669e55571adcb4066f1adf3425b3332cf855496c
Eric Kennes <erickennes9@gmail.com>
2025-07-03 15:15:40 -0400

    Big One

--- src/app/Stuff/W7/page.tsx
+++ src/app/Stuff/W7/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function W7() {
   return (
-    <main className="flex w-full">
-      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
-      <p> <img src="/images/W7/W7GIFFY.gif" alt="Description of Image6"></img></p>
+    <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
+      <div className="flex w-full max-w-[1080px] flex-col space-y-6">
+      <p> <img src="/images/W7/My Movie 7.mov" alt="Description of Image6"></img></p>
 
        <p className="text-xl leading-relaxed"> Week 7 and time do be flying!
 
