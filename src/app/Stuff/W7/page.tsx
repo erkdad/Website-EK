66a9c3fb7cab63550b26b2463b32492da2461785
Eric Kennes <erickennes9@gmail.com>
2025-07-03 15:02:46 -0400

    W7 Launch

---
+++ src/app/Stuff/W7/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function W7() {
+  return (
+    <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6"></p>
+
+        <p> <img src="/images/W7/W7GIFFY.gif" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed"> Week 7 and time be flyin! Spent 101% of time outdoors
+        </p>
+        <p className="text-xl leading-relaxed"> 07.03.2025 EK . Gettin Sun
+        </p>
+      </div>
+    </main>
+  );
+}
