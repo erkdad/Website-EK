0693b2cbfa6e7e4e4b4d334626ab9f14f95324f1
Eric Kennes <erickennes9@gmail.com>
2025-07-03 15:09:16 -0400

    W7 tryagain

--- src/app/Stuff/W7/page.tsx
+++ src/app/Stuff/W7/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function W7() {
   return (
     <main className="flex w-full">
-      <div className="flex w-full max-w-[2100px] flex-col space-y-6"></p>
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+      <p> <img src="/images/W7/W7GIFFY.gif" alt="Description of Image6"></img></p>
 
-        <p> <img src="/images/W7/W7GIFFY.gif" alt="Description of Image6"></img></p>
+       <p className="text-xl leading-relaxed"> Week 7 and time do be flying!
 
-        <p className="text-xl leading-relaxed"> Week 7 and time be flyin! Spent 101% of time outdoors
         </p>
-        <p className="text-xl leading-relaxed"> 07.03.2025 EK . Gettin Sun
+        <p className="text-xl leading-relaxed"> 07.03.2025 EK . Stayin curious!
         </p>
       </div>
     </main>
