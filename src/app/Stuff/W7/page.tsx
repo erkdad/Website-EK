2f1edea63790beea833708f1de616ba9f9cdbb16
Eric Kennes <erickennes9@gmail.com>
2025-07-04 10:11:46 -0400

    Text add

--- src/app/Stuff/W7/page.tsx
+++ src/app/Stuff/W7/page.tsx
@@ -12,6 +12,23 @@ export default function W7() {
        <p className="text-xl leading-relaxed"> Week 7 and time do be flying!
 
         </p>
+
+        <p className="text-xl leading-relaxed"> This week i tried to upload a full scale video to background some writing. i couldnt figfure it out. 
+          I know it should include a .gif file that is at least 1920px wide. online software has a topugh time exporting large gif images...
+        </p>
+
+        <p className="text-xl leading-relaxed"> in the video, there was going to be buttery images of this weekends trip to noganosh, an unorganized ontario park where portages
+          are the mode of transportaion. 3 days out, we melted under the sun and even sustained a few burns. Our campsite was on a pennisula on smokey lake.
+          2 portages and 2 canoes, 4 of us made it to the campsite where we saw turtles, went swimming to islands, rope swung,  floated above the underwater cities and drifted into the sunshine.
+          being my first portage trip, it was an outstanding success and i hope to get back out there again. maybe next time ill clean the fish to eat.
+        </p>
+        <p className="text-xl leading-relaxed"> for now, here are some pictures instead!
+        </p>
+        <p className="text-xl leading-relaxed"> since moving to collingwood, i have yet to get out painting in the streets and the highlands. after work i usually
+          put pressure on myself to explore or get active. a new practice in mind is to go out and paint the morning light! striking shadows as the sun is rising will be a great way
+          to start the day with colour meditation. i find when i paint in the evenings, i have it on my mind all day and have a tough time focusing on work or other tasks. in the morning
+          i think the freshness of the day will give my sight a clean canvas to throw some colour down.
+        </p>
         <p className="text-xl leading-relaxed"> 07.03.2025 EK . Stayin curious!
         </p>
       </div>
