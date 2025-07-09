05ef7d79d14f41ce29786efcbb65612a7a9325e8
Eric Kennes <erickennes9@gmail.com>
2025-07-09 18:58:41 -0400

    yessss

--- src/app/Stuff/W8/page.tsx
+++ src/app/Stuff/W8/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function W9() {
+export default function W8() {
   return (
     <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
       <div className="flex w-full max-w-[1080px] flex-col space-y-6">
