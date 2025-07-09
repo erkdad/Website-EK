59a06d2d4a788b19e7f8d9bf4013038437bf5dfa
Eric Kennes <erickennes9@gmail.com>
2025-07-09 19:19:48 -0400

    this is itttt

--- src/app/Stuff/W8/page.tsx
+++ src/app/Stuff/W8/page.tsx
@@ -4,6 +4,8 @@ export default function W8() {
   return (
      <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+
+         <p className="text-xl leading-relaxed"> W E E K 8 </p>
        <p className="text-xl leading-relaxed">This past week went by at a good pace. 
         It began, on this blog schedule, with some painting at blue mountain in the morning. 
 In about 60 minutes I came out with a 12x16 painting that I was happy with. 
@@ -17,6 +19,48 @@ I was getting overwhelmed with the sketch I had started and chose to give up sli
    other paintings I enjoy looking at! </p>
 
         
+<p> <img src="/images/W8/IMG_8603.jpg" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed">Time was also spent going to and from Grand Bend,
+   and some time with the family. Found the beach during some hot summer days and got my tan maxed out.
+    Willamina hit up the golf course for her second round and found some green! </p>
+
+    <p> <img src="/images/W8/810FD0A4-3B69-4A6F-B7AD-021DF78B8959-3798-0000023B633B4805.jpg" alt="Description of Image6"></img></p>
+    <p> <img src="/images/W8/SWAGGOLF.png" alt="Description of Image6"></img></p> 
+
+    <p className="text-xl leading-relaxed">Last night I rode the red bike through the mountains and found a good
+   state consciousness in the winding round and grave; paths in the trees! Riding downhill at 100km/h is quite a 
+   different feeling than riding a flat…</p>
+
+        <p> <img src="/images/W8/BIKERS.png" alt="Description of Image6"></img></p>
+
+        <p className="text-xl leading-relaxed">Also, the homie Ryan just drove to chicago to pick up his 240. I appreciate people who like what they like and are willing to push for it. I have been subtly looking at 
+   240 ads over the past week, check this shit out! Thew second photo is the last 240 ever made…</p>
+
+        <p> <img src="/images/W8/VOLVO.png" alt="Description of Image6"></img></p>
+
+<p className="text-xl leading-relaxed">This Friday is Willaminas birthday and I couldn't be more appreciative of her company over the past 9 months.
+   She has renewed my thoughts about life and the world that surrounds me! thank you WILLI !! </p> 
+
+        <p> <img src="/images/W8/HBDW2.png" alt="Description of Image6"></img></p>
+  
+
+        <p className="text-xl leading-relaxed">44.99360986514778, -79.20174297179162</p>
+
+        <p className="text-xl leading-relaxed">45.08814726293169, -79.2995799729436</p>
+
+        <p className="text-xl leading-relaxed">44.948481044108036, -79.33944682673935</p>
+
+        <p className="text-xl leading-relaxed">45.039578128506626, -79.30769281812185</p>
+
+        <p className="text-xl leading-relaxed">44.50044850855959, -80.21645260493801</p>
+
+        <p className="text-xl leading-relaxed">44.49911867512276, -80.21578225893549</p>
+
+
+
+      <p className="text-xl leading-relaxed"> 07.09.2025 EK . YUHH</p>
+
       </div>
      </main>
   );
