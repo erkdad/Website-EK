d745a7042270c303680adc17d64f77b85c2aa7d1
Eric Kennes <erickennes9@gmail.com>
2025-07-09 18:26:54 -0400

    W8

---
+++ src/app/Stuff/W8/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+export default function W9() {
+  return (
+    <main className="flex flex-col items-center justify-between p-4 sm:p-6 md:p-8 overflow-hidden mx-auto">
+      <div className="flex w-full max-w-[1080px] flex-col space-y-6">
+       <p className="text-xl leading-relaxed">This past week went by at a good pace. 
+
+It began, on this blog schedule, with some painting at blue mountain in the morning. 
+In about 60 minutes I came out with a 12x16 painting that I was happy with. 
+This gave me a high that lasted a long time. Simple is subject nature yet and expanded view
+, the image i left with was something to encourage more morning sessions. Today I went out 
+again to face the canvas and located myself around the collingwood yacht club. After 30 minutes, 
+I was getting overwhelmed with the sketch I had started and chose to give up slightly, yet a passerby
+ told me that it was great and I should keep painting. He was right, I should keep painting and try to
+  solve the issues I had put on my canvas. Realistically, the sketch was off and the painting would have 
+  struggled no matter what. This week I hope to realize perspective issues within my own work and learn from
+   other paintings I enjoy looking at! </p>
+
+<p className="text-xl leading-relaxed">Time was also spent going to and from Grand Bend,
+   and some time with the family. Found the beach during some hot summer days and got my tan maxed out.
+    Willamina hit up the golf course for her second round and found some green! </p>
+
+<p className="text-xl leading-relaxed">Last night I rode the red bike through the mountains and found a good
+   state consciousness in the winding round and grave; paths in the trees! Riding downhill at 100km/h is quite a 
+   different feeling than riding a flat…</p>
+
+<p className="text-xl leading-relaxed">This Friday is Willamina’s birthday and I couldn't be more appreciative of her company over the past 9 months.
+   She has renewed my thoughts about life and the world that surrounds me! thank you WILLI !! </p> 
+
+<p className="text-xl leading-relaxed">44.99360986514778, -79.20174297179162</p>
+
+<p className="text-xl leading-relaxed">45.08814726293169, -79.2995799729436</p>
+
+<p className="text-xl leading-relaxed">44.948481044108036, -79.33944682673935</p>
+
+<p className="text-xl leading-relaxed">45.039578128506626, -79.30769281812185</p>
+
+<p className="text-xl leading-relaxed">44.50044850855959, -80.21645260493801</p>
+
+<p className="text-xl leading-relaxed">44.49911867512276, -80.21578225893549</p>
+
+
+        <p> <img src="/images/W7/IMG_8567.jpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W7/IMG_8571.jpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W7/IMG_8580.jpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W7/IMG_8581.jpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W7/IMG_8582.jpg" alt="Description of Image6"></img></p>
+        <p> <img src="/images/W7/unnamed.jpg" alt="Description of Image6"></img></p>
+
+
+        <p className="text-xl leading-relaxed"> 07.09.2025 EK . Stayin curious!
+        </p>
+      </div>
+    </main>
+  );
+}
