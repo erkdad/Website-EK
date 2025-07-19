94d915586ae6c9320933b1483b95a3eb618df373
Eric Kennes <erickennes9@gmail.com>
2025-07-19 13:41:29 -0400

    no parties

--- src/app/Stuff/W9/page.tsx
+++ src/app/Stuff/W9/page.tsx
@@ -9,8 +9,7 @@ export default function W9() {
 
       <p> <img src="/images/W9/DSCF7207.JPG" alt="Description of Image6"></img></p>
          <p className="text-xl leading-relaxed"> Ended up uploading this on saturday because time was flying by this week. It began with my brothers Bachelor party
-            at the Blue Mountians. 13 guys in one house with 2 kegs of beer turns out to be a pretty good time. Each of us
-            did a keg stand on saturday night, where i went 18 seconds... i think i found the middle stat of the group.
+            at the Blue Mountians. 13 guys in one house with 2 kegs of beer turns out to be a pretty good time. I think i found the middle stat of the group.
             Each day this week has been fully loaded with fun things and thoughtful practice. Some days we went running in the mountains and climbing 
             at the corner, other days had us practicing handstands and setting up the fixie, and there were days spent out at old baldy! I found myself really enjoying 
             the vert running at Blue and Osler. The deep green trails and ups and downs make fitness very attractive.</p>
@@ -22,10 +21,6 @@ export default function W9() {
         
 <p> <img src="/images/W9/Landscape.png" alt="Description of Image6"></img></p>
 
-<p className="text-xl leading-relaxed">ABUNDANT TIME TO PARTI</p>
-
-<p> <img src="/images/W9/CHUG.png" alt="Description of Image6"></img></p>
-<p> <img src="/images/W9/PONG.png" alt="Description of Image6"></img></p>
 
 <p className="text-xl leading-relaxed">ABUNDANT TIME for TIME...</p>
 
