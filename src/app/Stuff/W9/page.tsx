024f99f803c7aa3aa674c81bdbd5c47d1c2e9d2c
Eric Kennes <erickennes9@gmail.com>
2025-07-19 09:53:21 -0400

    W9 LIVE

---
+++ src/app/Stuff/W9/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function W9() {
+  return (
+     <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+
+         <p className="text-xl leading-relaxed"> W_E_E_K_N_I_N_E </p>
+         <p className="text-xl leading-relaxed"> Ended up uploading this on saturday because time was flying by this week. It began with my brothers Bachelor party
+            at the Blue Mountians. 13 guys in one house with 2 kegs of beer turns out to be a pretty good time. Each of us
+            did a keg stand on saturday night, where i went 18 seconds... i think i found the middle stat of the group.
+            Each day this week has been fully loaded with fun things and thoughtful practice. Some days we went running in the mountains and climbing 
+            at the corner, other days had us practicing handstands and setting up the fixie, and there were days spent out at old baldy! I found myself really enjoying 
+            the vert running at Blue and Osler. The deep green trails and ups and downs make fitness very attractive.</p>
+       <p className="text-xl leading-relaxed"></p>
+
+      <p className="text-xl leading-relaxed"> A big thought this week is THE THOUGHT OF TIME AS ABUNDANCE rather than TIME AS LIMITATION.</p>
+       <p className="text-xl leading-relaxed"></p>
+
+        
+<p> <img src="/images/W8/IMG_8603.jpg" alt="Description of Image6"></img></p>
+
+
+      <p className="text-xl leading-relaxed"> 07.09.2025 EK . YUHH</p>
+
+      </div>
+     </main>
+  );
+}
--- src/app/Stuff/page.tsx
+++ src/app/Stuff/page.tsx
@@ -26,6 +26,14 @@ export default function Stuff() {
               onClick={() => router.push("/Stuff/Photos")}
             />
             <Image
+              src="/images/Week9_Logo.svg"
+              alt="Stuff"
+              width={200}
+              height={100}
+              className="cursor-pointer"
+              onClick={() => router.push("/Stuff/W9")}
+            /> 
+            <Image
               src="/images/Week8_Logo.svg"
               alt="Stuff"
               width={200}
