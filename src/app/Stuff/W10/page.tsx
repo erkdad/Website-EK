b6f61b82444de555a6896e8b16b7b02a175a4c1e
Eric Kennes <erickennes9@gmail.com>
2025-07-22 09:26:34 -0400

    w10 push

---
+++ src/app/Stuff/W10/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function W10() {
+  return (
+     <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+
+         <p className="text-xl leading-relaxed"> week10comingsoon... </p>
+
+
+
+      </div>
+     </main>
+  );
+}
