1b01b2c9c0c56a5bcadd5e93fa653ee826429930
Eric Kennes <erickennes9@gmail.com>
2025-07-25 16:50:39 -0400

    SAVE THAT SHIT

--- src/app/Stuff/W10/page.tsx
+++ src/app/Stuff/W10/page.tsx
@@ -5,11 +5,30 @@ export default function W10() {
      <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
 
-         <p className="text-xl leading-relaxed"> week10comingsoon... </p>
+         <p className="text-xl leading-relaxed"> W   E   E   K   10 </p>
 
+         <p className="text-xl leading-relaxed">This week we made it to the ultimate 10! *metaphorical pat on the shoulder* </p>
 
+<p className="text-xl leading-relaxed"> The whole purpose of these posts is to reflect on times that have passed so i dont move too quickly. A practice of slowing down and feeling the story of life.
+I definitely find use in that, a meditational weekly episode for my mind to chew on.
+ </p>
 
-         <p> The website for WindPro can be found <a href="https://www.emd-international.com/software/windpro"><span className="font-semibold">HERE</span></a></p>
+  <p className="text-xl leading-relaxed">I was reflecting on some good memories earlier this week; insert Europe 2022!</p>
+
+<p> <img src="/images/W10/DSCF4990.png" alt="Description of Image6"></img></p>
+<p> <img src="/images/W10/DSCF4985.png" alt="Description of Image6"></img></p>
+<p> <img src="/images/W10/DSCF4910.png" alt="Description of Image6"></img></p>
+<p> <img src="/images/W10/DSCF4759.png" alt="Description of Image6"></img></p>
+<p> <img src="/images/W10/DSCF4841.png" alt="Description of Image6"></img></p>
+<p> <img src="/images/W10/DSCF4999.png" alt="Description of Image6"></img></p>
+
+<p className="text-xl leading-relaxed">Over the past week, my mind has been set on learning the immense WindPro software to encourage the company to open business in wind siting. With the current age of software and online resources, anyone willing to spend the time on something they love can work their way into action. The payment method is time, attention, and focus. I hope I can answer questions like:  </p>
+<p className="text-xl leading-relaxed">How to optimize a wind resource area given the intended hub height?  </p>
+<p className="text-xl leading-relaxed">How to analyse wind data to show developers the best first steps to take in a major wind operation?</p>
+<p className="text-xl leading-relaxed">General project outlook size to accomplish the potential energy production of X MWh/y</p>
+         <p className="text-xl leading-relaxed">These are fun analysis questions to ponder when looking at the aerial imagery of the earth. how to harness wind to produce energy for the surrounding grid! Energy Transition in real time.</p>
+         
+         <p className="text-xl leading-relaxed"> The website for WindPro can be found <a href="https://www.emd-international.com/software/windpro"><span className="font-semibold">HERE</span></a></p>
 
       </div>
      </main>
