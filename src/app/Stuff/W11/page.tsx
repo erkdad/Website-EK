7fa648f6de6557cab8ad173cc795842380f765a1
Eric Kennes <erickennes9@gmail.com>
2025-08-01 16:42:02 -0400

    W11 ON

--- src/app/Stuff/W11/page.tsx
+++ src/app/Stuff/W11/page.tsx
@@ -5,8 +5,29 @@ export default function W11() {
      <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
 
-         <p className="text-xl leading-relaxed"> WEEK11COMINGSOON </p>
+         <p className="text-xl leading-relaxed"> WEEKeleven </p>
 
+
+         <p> <img src="/images/W11/Mom.png" alt="Description of Image6"></img></p>
+
+         <p className="text-xl leading-relaxed"> Another week in the books for Collingwood. This place is starting to feel like a home. I feel the presence of community and the orientation of my surroundings starting to make sense. This morning I drove home from Willaminas place and the welcoming embrace I feel from the highlands on the horizon is exciting. I really do appreciate moving here. </p>
+
+
+         <p> <img src="/images/W11/Felix.png" alt="Description of Image6"></img></p>
+
+
+
+         <p className="text-xl leading-relaxed"> I want to push the creative pursuits here and get to know those around me who feel the same. I am contemplating a plein air painting group that encourages all skill levels to come out and share creative times together. People of all styles can learn from one another, whether it is composition, colour, subject, or overall expression. Since I dont have a consistent schedule until September, I want to conceptualize an bi-weekly (or even weekly) event that has plein air artists meet in a common location and gives time for discussions and new introductions. I would love for everyone to share their ideal spots of work. I felt the art community of London with figure drawing and some of the painters I knew around town. Those were some of my favourite interactions!</p>
+
+
+
+         <p> <img src="/images/W11/IMG_8781.jpeg" alt="Description of Image6"></img></p>
+
+
+         <p className="text-xl leading-relaxed"> The past week had some day 1s come visit for some heavy planned action. We found ourselves at the Devils Glen for some rock torture. Toivenen set the meanest 10d i have ever crimped, had to give him a hug for that. Found a 5.8 with the first draw at approximately 20ft and NO STICK CLIP… After a short amount of terror the anchor was speed bagged. Following that we had a reunion run with Brian at Osler. The forest was golden and showered us with some heavy reflections which grew our boy bonds. see some of the results below;</p>
+         <p> <img src="/images/W11/4CB81D62-B5C7-4677-9020-27B09755D656-1290-00000033A641635F.jpeg" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W11/IMG_8766.jpeg" alt="Description of Image6"></img></p>
+         <p className="text-xl leading-relaxed"> 08.01.2025 flyinbytooooofast... be mindful.</p>
       </div>
      </main>
   );
