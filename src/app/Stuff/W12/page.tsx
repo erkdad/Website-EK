56d2926590f6bb2abdbb3b85b70eb70ccf83dcf1
Eric Kennes <erickennes9@gmail.com>
2025-08-17 20:42:14 -0400

    did it

--- src/app/Stuff/W12/page.tsx
+++ src/app/Stuff/W12/page.tsx
@@ -5,9 +5,15 @@ export default function W11() {
      <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
 
-         <p className="text-xl leading-relaxed"> WEEK tuwelv coming </p>
+         <p className="text-xl leading-relaxed"> WEEK tuwelv is below. </p>
 
-         <p> <img src="/images/W12/IMG_8971.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic1_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic6_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic3_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic4_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic2_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/pic5_12.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W12/Pic7_12.png" alt="Description of Image6"></img></p>
 
          <p className="text-xl leading-relaxed"> 08.08.2025 StAy Stoked !!</p>
       </div>
