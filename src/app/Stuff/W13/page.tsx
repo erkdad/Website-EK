e1ce2ee374e2bd14e4d47347f727b6cdb66a5f81
Eric Kennes <erickennes9@gmail.com>
2025-08-19 21:05:54 -0400

    yeeeeter

--- src/app/Stuff/W13/page.tsx
+++ src/app/Stuff/W13/page.tsx
@@ -8,7 +8,7 @@ export default function W11() {
          <p className="text-xl leading-relaxed"> WEEK thirtean is here </p>
 
          <p className="text-xl leading-relaxed"> This week I have been given the green light to orchestrate the beginnings of the windpro license for our company. To me this is just the starting of something incredible where land management meets in depth analysis of energy potential. Between now and the conception of this business venture, I hope to do readings on wake models, solar radiance studies and general field workings of current projects in the areas we are working.  </p>
-         <p className="text-xl leading-relaxed"> This Friday I will be leaving from London airport to Victoria BC to meet Willi! I'll be sure to save the articles on a readable device for some flight reads. </p>
+         <p className="text-xl leading-relaxed"> This Friday I will be leaving from London airport to Victoria BC to meet Willi! Ill be sure to save the articles on a readable device for some flight reads. </p>
          <p className="text-xl leading-relaxed"> Also, I was out painting this weekend somewhere near collingwood and had a bunch of locals walk by me working. They gave me some great comments and asked me questions like; Where are you from? How long have you been doing this? AND, do you teach? I then shared my ideas of starting a plein air painting group in the Collingwood area. I got great feedback and even some individuals saying that they would be looking for my post. One lady even told me to reach out to the printer of the newsflyer in Clarksburg. Ill have to draft a good post to initiate the group. </p>
          <p className="text-xl leading-relaxed"> Other painting thoughts that have occurred over the past week is the betterment of green mixing. Currently there are only 2 blues and 2 yellows that I use. Generally, on my palette I have 4 colours; 2 yellows and 1 blue and 1 white, so as to not bog down my thoughts in mixing consistent greens. Yet when out in the field I find the greens I see in nature are so diverse that there really should be 5 if not 6 colours on my palette to contribute to greens.  </p>
          <p className="text-xl leading-relaxed"> To my impression there should be; </p>
@@ -19,10 +19,10 @@ export default function W11() {
 
 
          <p className="text-xl leading-relaxed"> These variables will allow me to more easily mix a cool green in high value and also one in low value. Then the same ability to mix a warm green in high value and also in low value. This statement above is one that I still don't know is true but feel is the correct path forward. I am trying to source good green mixing theories online. My favourite painter, Marc Dalessio has a video online that breaks down greens in a natural format that is hard not to separate from something <a href="https://www.youtube.com/watch?v=IyJF0BycTlQ"><span className="font-semibold">SACRED</span></a>. Once online, I'm hoping to find other artists who pull greens in a way that my eye is attracted to. I'll put some below. </p>
-         <p className="text-xl leading-relaxed"> Some of the paintings within Kenny Harris' <a href="https://www.kenny-harris.com/Artist.asp?ArtistID=39381&Akey=QF4PTBG7&ajx=1#!Group1_Pf189993"><span className="font-semibold">IRELAND 2020</span></a> exhibit showcase very sophisticated green work.</p>
+         <p className="text-xl leading-relaxed"> Some of the paintings within Kenny Harris <a href="https://www.kenny-harris.com/Artist.asp?ArtistID=39381&Akey=QF4PTBG7&ajx=1#!Group1_Pf189993"><span className="font-semibold">IRELAND 2020</span></a> exhibit showcase very sophisticated green work.</p>
          <p className="text-xl leading-relaxed"> Another artist i find who has a briliant simplification of greens is Tim Horn. The impressionist technique in his use of <a href="https://www.timhornart.com/copy-of-available"><span className="font-semibold">GREENS</span></a>is quite eye watering.</p>
          
-          <p className="text-xl leading-relaxed"> More to come!</p>
+         <p className="text-xl leading-relaxed"> More to come!</p>
          <p className="text-xl leading-relaxed"> 08.13.2025 it is all green!</p>
       </div>
      </main>
