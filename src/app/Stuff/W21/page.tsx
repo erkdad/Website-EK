15228d86ad188975b4167d3a4ca74cc2ff9244cc
Eric Kennes <erickennes9@gmail.com>
2025-10-15 11:39:07 -0400

    21

---
+++ src/app/Stuff/W21/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function W21() {
+  return (
+     <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+
+         <p className="text-xl leading-relaxed">Week 21 is now here</p>
+
+         <p> <img src="/images/W21/Landscape.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W21/87558.png" alt="Description of Image6"></img></p>
+         <p> <img src="/images/W21/45823.png" alt="Description of Image6"></img></p>
+         
+      
+         <p className="text-xl leading-relaxed">10.15.2025 Patience and intention. Still a little behind.</p>
+      </div>
+     </main>
+  );
+}
