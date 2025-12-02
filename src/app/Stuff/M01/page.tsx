715a9cce24cca8f274329efc70f044b7bab2edf9
Eric Kennes <erickennes9@gmail.com>
2025-12-02 08:34:44 -0500

    plswrk

---
+++ src/app/Stuff/M01/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function M01() {
+  return (
+     <main className="flex w-full">
+      <div className="flex w-full max-w-[2100px] flex-col space-y-6">
+      <p className="text-xl leading-relaxed">Heyoo, new change of plans. Monthly mag posts! More time to establish the thought and art!
+         This is title Month 1 but realistically, this will be my 7th month of posting! Enjoy.</p>
+      <p> <img src="/images/M01/12.png" alt="Description of Image6"></img></p>
+      <p> <img src="/images/M01/34x2.png" alt="Description of Image6"></img></p>
+      <p> <img src="/images/M01/56.png" alt="Description of Image6"></img></p>
+      <p> <img src="/images/M01/78.png" alt="Description of Image6"></img></p>
+      <p> <img src="/images/M01/910.png" alt="Description of Image6"></img></p>
+   
+
+
+         
+      
+      <p className="text-xl leading-relaxed">12.02.2025</p>
+      </div>
+     </main>
+  );
+}
--- src/app/Stuff/page.tsx
+++ src/app/Stuff/page.tsx
@@ -26,6 +26,14 @@ export default function Stuff() {
               onClick={() => router.push("/Stuff/Photos")}
             />
             <Image
+              src="/images/Month1.svg"
+              alt="Stuff"
+              width={200}
+              height={100}
+              className="cursor-pointer"
+              onClick={() => router.push("/Stuff/M01")}    
+            />
+            <Image
               src="/images/Week24_Logo.svg"
               alt="Stuff"
               width={200}
