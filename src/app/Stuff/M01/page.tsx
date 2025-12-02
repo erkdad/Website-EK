641514b2630e25e67025fad70c92680ea7b4beca
Eric Kennes <erickennes9@gmail.com>
2025-12-02 09:02:37 -0500

    yogotti

--- src/app/Stuff/M01/page.tsx
+++ src/app/Stuff/M01/page.tsx
@@ -5,7 +5,7 @@ export default function M01() {
      <main className="flex w-full">
       <div className="flex w-full max-w-[2100px] flex-col space-y-6">
       <p className="text-xl leading-relaxed">Heyoo, new change of plans. Monthly mag posts! More time to establish the thought and art!
-         This is title Month 1 but realistically, this will be my 7th month of posting! Enjoy.</p>
+         This is titled Month 1 but realistically, this will be my 7th month of posting! Enjoy.</p>
       <p> <img src="/images/M01/12.png" alt="Description of Image6"></img></p>
       <p> <img src="/images/M01/34x2.png" alt="Description of Image6"></img></p>
       <p> <img src="/images/M01/56.png" alt="Description of Image6"></img></p>
